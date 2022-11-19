558bb1c0233edd7225ae9e0d48050e43af5633ba
filipepfarias <filipepfarias@alu.ufc.br>
2022-11-19 11:35:09 +0200

    WIP - Entropy production rate

--- CMESolver.jl
+++ CMESolver.jl
@@ -8,59 +8,76 @@ using DifferentialEquations: solve, ODEProblem, RK4
 using ProgressMeter
 
 path = "outputs/"*randstring(5)*"_"*Dates.format(now(),"yyyymmdd")
-mkdir(path)
+# mkdir(path)
 
 println("Building the CME operator...")
 comp_time = @elapsed begin
     model_nm = "2ABreaction"
     model = "reactions/"*model_nm*".jl";
     include(model);
-    cp(model,path*"/model.jl")
+    # cp(model,path*"/model.jl")
 end
 println("Computation time for the assemble of the operator: "*string(comp_time)*"s.")
 
 T = 0.0:.2:50.0;
 f(u,p,t) = A*u ;
+q(u,p,t) = A'*u
 
 begin
-    u0 = p₀[:];
-    p = u0;
+    uf = p₀[:];
+    p = uf;
+
+    pf = zeros(length(uf),length(T));
+    pb = zeros(length(uf),length(T));
 
     # flname = path*"/"*model_nm*"_t"*string(0);
     # jldsave(flname, p=p, t=0.0)
     # E = CMEMarginals(u0,𝗻ₖ,specie,flname,0.0)
 
-    marg_labels, marg, 𝔼, 𝕍ar, ℝ, Sk, 𝕊 = CMEStatistics(u0,𝗻ₖ,specie);
+    # marg_labels, marg, 𝔼, 𝕍ar, ℝ, Sk, 𝕊 = CMEStatistics(u0,𝗻ₖ,specie);
     
     println("Saving on "*path*".")
 
-    flname = path*"/"*model_nm*"_statistics_t"*string(0);
-    jldsave(flname, specie=specie,
-        marg_labels=marg_labels, 
-        marg=marg, E=𝔼, Var=𝕍ar, R=ℝ, Sk=Sk, S=𝕊, t=0, T=T)
+    # flname = path*"/"*model_nm*"_statistics_t"*string(0);
+    # jldsave(flname, specie=specie,
+    #     marg_labels=marg_labels, 
+    #     marg=marg, E=𝔼, Var=𝕍ar, R=ℝ, Sk=Sk, S=𝕊, t=0, T=T)
+    pf[:,1] = uf;
 
     pgres = Progress(length(T)-1; showspeed=true, desc="Solving the CME...")
 
     for iT in eachindex(T)[1:end-1]
-        global u0, flname, marg_labels, marg, 𝔼, 𝕍ar, ℝ, Sk, 𝕊
-        prob = ODEProblem(f,u0, (T[iT],T[iT+1]));
-        sol = solve(prob, RK4();dt= .5/15, saveat=T[iT+1],adaptive=false);
-        u0 = sol.u[end]/sum(sol.u[end]);
+        global uf, flname, marg_labels, marg, 𝔼, 𝕍ar, ℝ, Sk, 𝕊, sol
+        prob = ODEProblem(f,uf, (T[iT],T[iT+1]));
+        sol = solve(prob, RK4();dt= .5/20, saveat=T[iT+1],adaptive=false);
+        uf = sol.u[end]/sum(sol.u[end]);
         # u0 = sol.u[end]
+        pf[:,iT+1] = uf;
 
         # flname = path*"/"*model_nm*"_t"*string(iT);
         # jldsave(flname, p=u0, t=T[iT+1])
-        marg_labels, marg, 𝔼, 𝕍ar, ℝ, Sk, 𝕊 = CMEStatistics(u0,𝗻ₖ,specie)
+        # marg_labels, marg, 𝔼, 𝕍ar, ℝ, Sk, 𝕊 = CMEStatistics(u0,𝗻ₖ,specie)
 
-        flname = path*"/"*model_nm*"_statistics_t"*string(iT);
-        jldsave(flname, specie=specie,
-        marg_labels=marg_labels, 
-        marg=marg, E=𝔼, Var=𝕍ar, R=ℝ, Sk=Sk, S=𝕊, t=T[iT], T=T)
+        # flname = path*"/"*model_nm*"_statistics_t"*string(iT);
+        # jldsave(flname, specie=specie,
+        # marg_labels=marg_labels, 
+        # marg=marg, E=𝔼, Var=𝕍ar, R=ℝ, Sk=Sk, S=𝕊, t=T[iT], T=T)
 
-        ProgressMeter.next!(pgres)
+        # ProgressMeter.next!(pgres)
     end
+
+    ub = uf;
+    for iT in eachindex(T)[1:end-1]
+        global ub, flname, marg_labels, marg, 𝔼, 𝕍ar, ℝ, Sk, 𝕊, sol
+        prob = ODEProblem(q,ub, (T[iT],T[iT+1]));
+        sol = solve(prob, RK4();dt= .5/30, save_on = false, adaptive=false);
+        ub = sol.u[end]/sum(sol.u[end]);
+        pb[:,iT+1] = ub;
+    end
+
+    SS = [sum(pf[:,iT] .* ( log.(pf[:,iT]) - log.(pb[:,iT]) )) for iT in eachindex(T)]
 end
 
 # Plotting
 println("Saving plots...")
-include("misc_plotting2.jl")
+# include("misc_plotting2.jl")
